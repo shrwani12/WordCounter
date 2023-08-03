627856aa19cd9e1c2b29aef016e9e8ffa2a5144a
birajdar shrawani yeshwant <140878074+shrwani12@users.noreply.github.com>
2023-08-03 15:26:40 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,29 @@
+class Example 
+{
+	int x = 10;
+	int y = 30;
+}
+class Test{
+    public static void main(String[] args) 
+	 {
+		int p = 50;
+		int q = m1();
+		 
+		 String s1 = "a";
+		 String s2 = new String("a");
+
+		 Example e = new Example();
+		 System.out.println(e.x);
+		  System.out.println(e.y);
+
+		   System.out.println(p);
+		    System.out.println(q);
+
+
+
+
+	 }
+	 static int m1(){
+		return 60;
+	 }
+}
